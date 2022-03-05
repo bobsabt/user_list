8eb17be848fd1a0b2fbcd3f90a2ad483157bb163
bobsabt <bodai.barbara@gmail.com>
2022-03-05 17:57:19 +0100

    status update

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
-const User = ({firstname, lastname, createdat, active}) => {
+const User = ({firstname, lastname, createdat, status}) => {
+
+    const [active, setActive] = React.useState(status)
+
   return (
     <>
         <tr>
             <td className={active ? "": "text-locked"}>{firstname}</td>
             <td className={active ? "": "text-locked"}>{lastname}</td>
             <td className={active ? "": "text-locked"}>{createdat}</td>
+            <td>       
+                <button className={active ? "on": "off"} >{active ? "ACTIVE": "LOCKED"}</button>
+            </td>
         </tr>
     </>
   )
